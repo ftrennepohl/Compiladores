fcdc911247ff910c6867c4734a99c9b38d4c1ab7
ftrennepohl <ftrennepohl@gmail.com>
2024-06-30 18:05:20 -0300

    .

--- afd.py
+++ afd.py
@@ -5,18 +5,20 @@ class AFD(AFND):
     def __init__(self) -> None:
         self.afd = AFND()
         super().__init__()
+        self.afd.fromFile('input.txt')
         self.__determine()
     
     def __determine(self):
         while True:
             new_state = None
-            transition_nd = None
+            nd_transition = None
             for state in self.afd.states.values():
                 non_deterministic = False
                 for transition in state.transitions.values(): # para cada transicao por um simbolo
                     if (transition is not None and len(transition) > 1): # se tiver >1 transicao por simbolo
-                        transition_nd = [t for t in transition]
-                        new_state = State(id=f"[{', '.join(transition)}]")
+                        nd_transition = [t for t in transition]
+                        state_ids = [str(s.id) for s in transition]
+                        new_state = State(id=f"[{', '.join(state_ids)}]")
                         transition.clear()
                         transition.append(new_state.id)
                         non_deterministic = True
@@ -24,12 +26,13 @@ class AFD(AFND):
                 if(non_deterministic): break
             if (new_state is not None):
                 self.afd.addState(new_state)
-            else: break # determinizado
-            for state in transition_nd: # adiciona transicoes dos states que compoem ao novo state
-                for simbolo, transition in self.states[state].transitions.items():
-                    for state_destino in transition:
-                        self.states[new_state.id].adcTransicao(simbolo, state_destino)
-                if (self.states[state].final):
+            else:
+                break # determinizado
+            for state in nd_transition: # adiciona transicoes dos states que compoem ao novo state
+                for symbol, transition in self.afd.states[state.id].transitions.items():
+                    for target_state in transition:
+                        self.afd.states[new_state.id].addTransition(symbol, target_state)
+                if (self.afd.states[state.id].final):
                     new_state.final = True
-            for state in transition_nd:
-                self.states.pop(state)
+            for state in nd_transition:
+                self.afd.states.pop(state.id)
--- main.py
+++ main.py
@@ -2,6 +2,5 @@ from afd import AFD
 from afnd import AFND
 
 afd = AFD()
-afd.fromFile('input.txt')
 #afnd.printStates()
 afd.printWithError()
